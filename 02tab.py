453feb08c0b71baf2170642fdc5326677ad0fac0
tx621615 <710587152@qq.com>
2020-10-21 16:28:45 +0800

    third change

--- 02tab.py
+++ 02tab.py
@@ -1,10 +1,10 @@
-#测试缩进,python中没有大括号，通过相同的缩进表示同一个代码块
+# 测试缩进,python中没有大括号，通过相同的缩进表示同一个代码块
 if True:
     print("Answer")
     print("True")
     
 else:
     print("Answer")
-print("False")  #缩进不同则不在同一个块中
+print("False")  # 缩进不同则不在同一个块中
 
 
